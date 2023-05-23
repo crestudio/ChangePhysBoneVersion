445305db3beadbc46e39f2b2ad7f0349852ebaa1
Macchiato <simppp@gmail.com>
2023-05-23 20:35:28 +0900

    주석 추가

--- Assets/ChangePhysBoneVersion.cs
+++ Assets/ChangePhysBoneVersion.cs
@@ -18,6 +18,7 @@ namespace VRSuya.ChangePhysBoneVersion {
     public class ChangePhysBoneVersion : EditorWindow {
 
 		[MenuItem("Tools/VRSuya/PhysBone/Change All PhysBone Version to 1.0")]
+		/// <summary>Scene에 존재하는 모든 PhysBone을 1.0으로 변경합니다</summary>
 		static void ChangePhysBoneVersionTo1_0() {
 			List<VRCPhysBone> PhysBoneComponents = GetPhysBoneComponents();
 			foreach (VRCPhysBone PhysBone in PhysBoneComponents) {
@@ -28,6 +29,7 @@ namespace VRSuya.ChangePhysBoneVersion {
 		}
 
 		[MenuItem("Tools/VRSuya/PhysBone/Change All PhysBone Version to 1.1")]
+		/// <summary>Scene에 존재하는 모든 PhysBone을 1.1으로 변경합니다</summary>
 		static void ChangePhysBoneVersionTo1_1() {
 			List<VRCPhysBone> PhysBoneComponents = GetPhysBoneComponents();
 			foreach (VRCPhysBone PhysBone in PhysBoneComponents) {
@@ -38,6 +40,7 @@ namespace VRSuya.ChangePhysBoneVersion {
 		}
 
 		[MenuItem("Tools/VRSuya/PhysBone/Debug All PhysBone Version")]
+		/// <summary>Scene에 존재하는 모든 PhysBone의 버전을 Unity Console에 출력합니다</summary>
 		static void DebugLogPhysBoneComponets() {
 			List<VRCPhysBone> PhysBoneComponents = GetPhysBoneComponents();
 			foreach (VRCPhysBone PhysBone in PhysBoneComponents) {
@@ -45,6 +48,8 @@ namespace VRSuya.ChangePhysBoneVersion {
 			}
 		}
 
+		/// <summary>Scene에 존재하는 모든 PhysBone의 버전을 Unity Console에 출력합니다</summary>
+		/// <returns>Scene에 존재하는 모든 PhysBone 컴포넌트 리스트</returns>
 		static List<VRCPhysBone> GetPhysBoneComponents() {
 			return SceneManager.GetActiveScene().GetRootGameObjects().SelectMany(gameObject => gameObject.GetComponentsInChildren<VRCPhysBone>(true)).ToList();
 		}
